a829efdb882683cd755ca591c34ee4b657b606af
Fabio Maggio <fabio@infoweb.be>
2014-11-06 10:49:54 +0100

    Add dynamic editor options

--- views/page-partial/_language_tab.php
+++ views/page-partial/_language_tab.php
@@ -8,23 +8,7 @@ use mihaildev\ckeditor\CKEditor;
     ]); ?>
     
     <?= $form->field($model, "[{$model->language}]content")->widget(CKEditor::className(), [
-        'editorOptions' => [
-            'height' => 300,
-            'toolbarGroups' => [
-                ['name' => 'clipboard', 'groups' => ['mode','undo', 'selection', 'clipboard','doctools']],
-                ['name' => 'editing', 'groups' => ['tools']],
-                ['name' => 'paragraph', 'groups' => ['templates', 'list', 'indent', 'align']],
-                ['name' => 'insert'],
-                ['name' => 'basicstyles', 'groups' => ['basicstyles', 'cleanup']],
-                ['name' => 'colors'],
-                ['name' => 'links'],
-                ['name' => 'others'],
-            ],
-            'removeButtons' => 'Smiley,Iframe',
-            'rows' => 20,
-            'name' => "PagePartialLang[{$model->language}][content]",
-            'preset' => 'custom',
-        ],
-        
+        'name' => "PagePartialLang[{$model->language}][content]",
+        'editorOptions' => Yii::$app->getModule('cms')->getCKEditorOptions(),        
     ]); ?>
 </div>
