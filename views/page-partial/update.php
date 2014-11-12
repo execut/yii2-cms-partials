5025d3879bb9f11077fcca39e40ba873ff4e947b
Ruben Heymans <ruben.heymans@gmail.com>
2014-11-12 13:36:40 +0100

    Remove 'view' view

--- views/page-partial/update.php
+++ views/page-partial/update.php
@@ -8,7 +8,7 @@ $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => Yii::t('infoweb/partials', 'Page Partial'),
 ]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('infoweb/partials', 'Page Partials'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['update', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="page-partial-update">
