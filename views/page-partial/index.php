c3ccc86c29919a48be7723b7fefee606a9baabb5
Ruben Heymans <ruben.heymans@gmail.com>
2014-11-12 09:34:39 +0100

    Update gridview

--- views/page-partial/index.php
+++ views/page-partial/index.php
@@ -48,6 +48,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'floatHeader' => true,
         'floatHeaderOptions' => ['scrollingTop' => 88],
         'hover' => true,
+        'export' => false,
     ]); ?>
     <?php Pjax::end(); ?>
 
