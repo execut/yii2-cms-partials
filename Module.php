e2ad7d6c9f7f3728b506c9292e35fc48a37fbd6e
Ruben Heymans <ruben.heymans@gmail.com>
2015-10-27 11:10:15 +0100

    Add content duplication plugin

--- Module.php
+++ Module.php
@@ -2,9 +2,17 @@
 
 namespace infoweb\partials;
 
+use Yii;
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'infoweb\partials\controllers';
+
+    /**
+     * Allow content duplication with the "duplicateable" plugin
+     * @var boolean
+     */
+    public $allowContentDuplication = true;
 
     /**
      * Module specific configuration of the ckEditor
@@ -19,6 +27,11 @@ class Module extends \yii\base\Module
         parent::init();
         
         // initialize the module with the configuration loaded from config.php
-        \Yii::configure($this, require(__DIR__ . '/config.php'));
+        Yii::configure($this, require(__DIR__ . '/config.php'));
+
+        // Content duplication is only possible if there is more than 1 app language
+        if (isset(Yii::$app->params['languages']) && count(Yii::$app->params['languages']) == 1) {
+            $this->allowContentDuplication = false;
+        }
     }
 }
--- views/page-partial/_default_language_tab.php
+++ views/page-partial/_default_language_tab.php
@@ -5,11 +5,13 @@ use yii\helpers\ArrayHelper;
 <div class="tab-content language-tab">
     <?= $form->field($model, "[{$model->language}]title")->textInput([
         'maxlength' => 255,
-        'name' => "PagePartialLang[{$model->language}][title]"
+        'name' => "PagePartialLang[{$model->language}][title]",
+        'data-duplicateable' => Yii::$app->getModule('pages')->allowContentDuplication ? 'true' : 'false',
     ]); ?>
     
     <?= $form->field($model, "[{$model->language}]content")->widget(CKEditor::className(), [
         'name' => "PagePartialLang[{$model->language}][content]",
         'editorOptions' => ArrayHelper::merge(Yii::$app->getModule('cms')->getCKEditorOptions(), Yii::$app->getModule('partials')->ckEditorOptions),
+        'options' => ['data-duplicateable' => Yii::$app->getModule('partials')->allowContentDuplication ? 'true' : 'false'],
     ]); ?>
 </div>
