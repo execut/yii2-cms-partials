95709cc0e0173a5335290e0f08075cd508032c87
Ruben Heymans <ruben.heymans@gmail.com>
2015-11-04 10:35:55 +0100

    Add rtl support

--- views/page-partial/_default_language_tab.php
+++ views/page-partial/_default_language_tab.php
@@ -1,6 +1,7 @@
 <?php
 use mihaildev\ckeditor\CKEditor;
 use yii\helpers\ArrayHelper;
+use infoweb\cms\helpers\LanguageHelper;
 ?>
 <div class="tab-content language-tab">
     <?= $form->field($model, "[{$model->language}]title")->textInput([
@@ -11,7 +12,7 @@ use yii\helpers\ArrayHelper;
     
     <?= $form->field($model, "[{$model->language}]content")->widget(CKEditor::className(), [
         'name' => "PagePartialLang[{$model->language}][content]",
-        'editorOptions' => ArrayHelper::merge(Yii::$app->getModule('cms')->getCKEditorOptions(), Yii::$app->getModule('partials')->ckEditorOptions),
+        'editorOptions' => ArrayHelper::merge(Yii::$app->getModule('cms')->getCKEditorOptions(), Yii::$app->getModule('partials')->ckEditorOptions, (LanguageHelper::isRtl($model->language)) ? ['contentsLangDirection' => 'rtl'] : []),
         'options' => ['data-duplicateable' => Yii::$app->getModule('partials')->allowContentDuplication ? 'true' : 'false'],
     ]); ?>
 </div>
