47f9de87ddfb471a375354ad4e70d22941286e61
user <user@email.com>
2016-11-06 20:54:25 +0300

    Postgresql adaptation

--- migrations/m140930_065454_init.php
+++ migrations/m140930_065454_init.php
@@ -5,39 +5,43 @@ use yii\db\Migration;
 
 class m140930_065454_init extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $tableOptions = null;
-        
+
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
-        
+
+        if ($this->db->driverName === 'pgsql') {
+            $this->execute('CREATE TYPE page_partials_type AS ENUM (\'system\',\'user-defined\')');
+        }
+
         // Create 'page_partials' table
         $this->createTable('{{%page_partials}}', [
-            'id'                    => Schema::TYPE_PK,
-            'type'                  => "ENUM('system','user-defined') NOT NULL DEFAULT 'user-defined'",
-            'name'                  => Schema::TYPE_STRING . '(255) NOT NULL',
-            'created_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'id'                    => $this->primaryKey(),
+            'type'                  => 'page_partials_type NOT NULL DEFAULT \'user-defined\'',
+            'name'                  => $this->string()->notNull(),
+            'created_at'            => $this->integer()->notNull()->unsigned(),
+            'updated_at'            => $this->integer()->notNull()->unsigned(),
         ], $tableOptions);
-        
+
         // Create 'page_partials_lang' table
         $this->createTable('{{%page_partials_lang}}', [
-            'page_partial_id'       => Schema::TYPE_INTEGER . ' NOT NULL',
-            'language'              => Schema::TYPE_STRING . '(10) NOT NULL',
-            'title'                  => Schema::TYPE_STRING . '(255) NOT NULL',
-            'content'               => Schema::TYPE_TEXT . ' NOT NULL',
-            'created_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL'
+            'page_partial_id'       => $this->integer()->notNull(),
+            'language'              => $this->string(10)->notNull(),
+            'title'                  => $this->string()->notNull(),
+            'content'               => $this->text()->notNull(),
+            'created_at'            => $this->integer()->notNull()->unsigned(),
+            'updated_at'            => $this->integer()->notNull()->unsigned()
         ], $tableOptions);
-        
+
         $this->addPrimaryKey('page_partial_id_language', '{{%page_partials_lang}}', ['page_partial_id', 'language']);
-        $this->createIndex('language', '{{%page_partials_lang}}', 'language');
+        $this->createIndex('language_i', '{{%page_partials_lang}}', 'language');
         $this->addForeignKey('FK_PAGE_PARTIALS_LANG_PAGE_PARTIAL_ID', '{{%page_partials_lang}}', 'page_partial_id', '{{%page_partials}}', 'id', 'CASCADE', 'RESTRICT');
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->dropTable('page_partials_lang');
         $this->dropTable('page_partials');
--- migrations/m151118_110728_add_url_field.php
+++ migrations/m151118_110728_add_url_field.php
@@ -5,12 +5,12 @@ use yii\db\Migration;
 
 class m151118_110728_add_url_field extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $this->addColumn('{{%page_partials_lang}}', 'url', Schema::TYPE_STRING.'(255) NOT NULL');
     }
 
-    public function down()
+    public function safeDown()
     {
         $this->dropColumn('{{%page_partials_lang}}', 'url');
     }
